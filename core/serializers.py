8f454e960c9142400acbfdfe01bc4fb4c7a19a2f
arnab-afk <arnabbhowmik019@gmail.com>
2024-05-30 01:17:31 +0530

    signup and login routes implemented

--- core/serializers.py
+++ core/serializers.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
-        model =User
-        fields=['id','username','password','email']
+        model = User 
+        fields = ['id', 'username', 'password', 'email']
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from django.urls import path,re_path
+from django.urls import re_path
+
 from . import views
 
 urlpatterns = [
-   re_path('login',views.login),
-   re_path('singup',views.signup),
-   re_path('test_token',views.test_token)
+    re_path('signup', views.signup),
+    re_path('login', views.login),
+    re_path('test_token', views.test_token),
 ]
